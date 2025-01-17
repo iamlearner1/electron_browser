7ae8a26ad57c8250544be9eeed5b295e0171d058
sunny shiva <sunnyshiva@sunnys-MacBook-Air.local>
2025-01-17 14:52:57 +0530

    second version with browser like search bar and screen recording feature

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electron", {
-  // Print function
-  print: (arg) => ipcRenderer.invoke("print", arg),
-});
